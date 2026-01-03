084438d19d08702d121196a0c5d0d921048940ae
pipefleurs87-sudo <pipefleurs87@gmail.com>
2026-01-03 02:23:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
-st.title("MORALOGY ENGINE")
-st.success("✅ Streamlit está ejecutando app.py desde la raíz")
-st.write("Si ves esto, el problema estaba resuelto.")
+st.write("HELLO WORLD")
