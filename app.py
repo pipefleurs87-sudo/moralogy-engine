fe614666e1623f2f9cc3d170c3c99f3dad7d93ed
pipefleurs87-sudo <pipefleurs87@gmail.com>
2026-01-03 02:26:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("HELLO WORLD")
+st.set_page_config(layout="wide")
+
+st.markdown("## Moralogy Engine")
+
+st.components.v1.html(
+    "<h3>UI EMBED OK</h3><p>Streamlit renderiza HTML.</p>",
+    height=200
+)
