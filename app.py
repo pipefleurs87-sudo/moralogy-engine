a6c59c4e3731fcb93ecdbce6e49456c6b059305a
pipefleurs87-sudo <pipefleurs87@gmail.com>
2026-01-03 02:20:48 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("MORALOGY ENGINE")
+st.success("✅ Streamlit está ejecutando app.py desde la raíz")
+st.write("Si ves esto, el problema estaba resuelto.")
